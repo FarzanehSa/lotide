8c1a940200116a534b0669a4714e749e205597fc
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-01 15:17:35 -0800

    update middle function, implement countOnly

--- middle.js
+++ middle.js
@@ -27,7 +27,7 @@ const middle = function(array) {
     // [1,2,3] --> [2]  - [1,2,3,4] --> [3]
     if (Number.isInteger(mid)) {
       // for [1,2,3,4] we need 2 as well
-      // add it to the left of array --> [2,3]
+      // add it to the left side of array --> [2,3]
       middleArray.unshift(array[mid - 1]);
     }
   }
