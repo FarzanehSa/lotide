d668690d4ca112de33ab027e2d860ffbd0159798
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-15 15:06:31 -0800

    setup mocha & chai for eqArray

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,15 +1,42 @@
-const {assertEqual} = require('../assertEqual');
+const assert = require('chai').assert;
 const {eqArrays} = require('../eqArrays');
 
-assertEqual(eqArrays([1,2,3],[1,2,3]),true);
-assertEqual(eqArrays(['1','2','3'],['1','2','3']),true);
-assertEqual(eqArrays([],[]),true);
-assertEqual(eqArrays(['abc','def'],['abc','def']),true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true);
-assertEqual(eqArrays([1,2,3],[1,2,'3']),false);
-assertEqual(eqArrays([1,2,3],[1,2]),false);
-assertEqual(eqArrays([1],[]),false);
-assertEqual(eqArrays([1,2,3],[1,2,'a']),false);
-assertEqual(eqArrays([1,2,3],[3,2,1]),false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false);
+describe('#eqArrays', () => {
+
+  it('[] = []', () => {
+    assert.isTrue(eqArrays([],[]));
+  })
+  
+  it('[1,2,3] = [1,2,3]', () => {
+    assert.isTrue(eqArrays([1,2,3],[1,2,3]));
+  })
+  
+  it("['abc','def'] = ['abc','def']", () => {
+    assert.isTrue(eqArrays(['abc','def'],['abc','def']));
+  })
+  
+  it("[[2, 3], ['a']] = [[2, 3], ['a']]", () => {
+    assert.isTrue(eqArrays([[2, 3], ['a']], [[2, 3], ['a']]));
+  })
+  
+  it('[1] != []', () => {
+    assert.isFalse(eqArrays([1],[]));
+  })
+  
+  it("[1,2,3] != [1,2,'3']", () => {
+    assert.isFalse(eqArrays([1,2,3],[1,2,'3']));
+  })
+  
+  it("[1,2,3] != [1,2]", () => {
+    assert.isFalse(eqArrays([1,2,3],[1,2]));
+  })
+  
+  it("[3,2,1] != [1,2,3]", () => {
+    assert.isFalse(eqArrays([3,2,1],[1,2,3]));
+  })
+  
+  it("[[2, 3], [4]] != [[2, 3], 4]", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4]));
+  })
+
+})
